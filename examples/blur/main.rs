ce36503cc4abd35c6f24b2bb8c24d3d7e52ee657
Ryan Naddy <untuned20@gmail.com>
2025-12-01 08:32:20 -0600

    Added ability to pass layer to filter/adjustment function

--- examples/blur/main.rs
+++ examples/blur/main.rs
@@ -1,36 +1,18 @@
-use abra::abra_core::Star;
 use abra::adjustments::prelude::*;
 use abra::canvas::prelude::*;
 use abra::filters::prelude::*;
-use abra::options::prelude::*;
-use abra::prelude::*;
 
 const FILE: &str = "assets/kelsey.jpg";
 // const FILE: &str = "assets/bikini.jpg";
 // const OUT_FILE: &str = "out/blur.png";
 
 fn main() {
-  let mut image = Image::new_from_path(FILE);
-
   let canvas = Canvas::new_from_path("Kelsey", FILE, None);
-  // .add_layer_from_path("Kelsey", FILE, None);
   if let Some(mut layer) = canvas.get_layer_by_name("Kelsey") {
     noise::despeckle(&mut layer, None);
     color::auto_tone(&mut layer, None);
     color::auto_color(&mut layer, None);
-    blur::gaussian_blur(&mut layer, 20, None);
+    // blur::gaussian_blur(&mut layer, 20, None);
     canvas.save("out/focus-blur.png", None);
-    return;
   }
-
-  let area = Star::new().fit((500, 500)).with_feather(50);
-  let options = ApplyOptions::new().with_area(area.clone().with_feather(10));
-  // levels::contrast(&mut image, 100, None);
-  // noise::despeckle(&mut image, None);
-  // color::auto_tone(&mut image, None);
-  // color::auto_color(&mut image, None);
-  // levels::photo_filter_preset(&mut image, FilterType::WarmingDark, 0.25, None);
-  // smooth::smooth_skin(&mut image, 0.5, None);
-  // levels::saturation(&mut image, 150, None);
-  image.save("out/focus-blur.png", None);
 }
